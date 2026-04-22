18fd886e3839c20cabaa153f31e67a6cdf6d59a9
23521155 <23521155@gm.uit.edu.vn>
2026-04-23 00:54:13 +0700

    Fix metadata exam detail page

--- FE/src/app/[lang]/exams/page.tsx
+++ FE/src/app/[lang]/exams/page.tsx
@@ -3,9 +3,10 @@ import { getDictionary } from '@/src/utils/dictionaries';
 import type {Locale} from '@/src/utils/i18n'
 import { examService } from '@/src/services/examService';
 import {notFound} from "next/navigation";
+import type { Metadata } from 'next';
+const baseUrl = process.env.NEXT_PUBLIC_APP_URL;
 
-
-export async function generateMetadata({params}: { params: Promise<{ lang: string }>}) {
+export async function generateMetadata({params}: { params: Promise<{ lang: string }>}) : Promise<Metadata> {
     const { lang } = await  params;
     const t = await getDictionary(lang as Locale)
     const [examsData, categoriesData] = await Promise.all([
@@ -14,11 +15,52 @@ export async function generateMetadata({params}: { params: Promise<{ lang: strin
     ]);
 
     if(!examsData || !categoriesData) {
-        return notFound();
+        return {
+            title: 'Thư viện đề thi',
+            description: 'Đang cập nhật dữ liệu...'
+        };
     }
+    const title = `${t.examLibrary} - Tổng hợp ${examsData.length}+ đề thi IT`;
+    const categoryKeywords = categoriesData
+        .map(cat => t[`cat_${cat.id}` as keyof typeof t] || cat.name)
+        .slice(0, 8) // Lấy tối đa 8 category nổi bật nhất để tránh description quá dài
+        .join(', ');
+    const description = `${t.examLibDesc}. Luyện thi các chứng chỉ: ${categoryKeywords}... Cập nhật mới nhất.`;
 
+    const topCategories = categoriesData.slice(0,10).map(cat => cat.name.toLowerCase());
     return {
-        title: ``,
+        title: title,
+        description: description,
+        keywords: ["luyện thi IT Passport",
+            "đề thi IT Passport tiếng Việt",
+            "luyện thi FE",
+            "đề thi FE tiếng Việt",
+            "thi thử IT Passport online",
+            "thi thử FE online",
+            "chứng chỉ IT Nhật Bản",
+            "sát hạch VITEC",
+            "giải đề FE có lời giải",
+            "từ vựng IT Passport",
+            ...topCategories,
+
+        ],
+        openGraph: {
+            title: title,
+            description: description,
+            url: `${baseUrl}/${lang}/exams`,
+            siteName: "IT Shiken",
+            images: {
+                url: `/thumbnail.jpg`,
+                width: 1200,
+                height: 630,
+                alt: "IT Shiken",
+            },
+            locale: lang === 'vi' ? 'vi_VN' : lang === 'ja' ? 'ja_JP' : 'en_US',
+            phoneNumbers: "0903571094",
+            emails: "nguyenletuanphi910.2019@gmail.com",
+            type: "website",
+            countryName: "Việt Nam"
+        },
         alternates: {
             canonical: `https://itshiken.io.vn/${lang}/exams`,
             languages: {
