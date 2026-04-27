887261dc407a85c570d199b46a9861d6347b1469
taimv2405 <taimv2405@gmail.com>
2026-04-27 19:41:51 +0700

    feat: increase participants on every submissions

--- BE/exam-service/src/main/java/com/edu/exam/repositories/ExamRepository.java
+++ BE/exam-service/src/main/java/com/edu/exam/repositories/ExamRepository.java
@@ -3,6 +3,8 @@ package com.edu.exam.repositories;
 import com.edu.exam.entities.Exam;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -35,4 +37,8 @@ public interface ExamRepository extends JpaRepository<Exam, String> {
 
     @EntityGraph(attributePaths = {"category", "questions"})
     Optional<Exam> findById(String id);
+
+    @Modifying(flushAutomatically = true, clearAutomatically = true)
+    @Query("UPDATE Exam e SET e.participants = e.participants + 1 WHERE e.id = :examId")
+    int incrementParticipants(@Param("examId") String examId);
 }
