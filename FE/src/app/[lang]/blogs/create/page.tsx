d32813e5983042d2f384f8579769430d199755cb
23521155 <23521155@gm.uit.edu.vn>
2026-05-01 16:36:59 +0700

    Create 2 blog

--- FE/src/app/[lang]/blogs/create/page.tsx
+++
@@ -1,5 +0,0 @@
-import { CreateBlog } from '@/src/views/CreateBlog';
-
-export default function Page() {
-    return <CreateBlog />;
-}
--- FE/src/app/[lang]/blogs/page.tsx
+++ FE/src/app/[lang]/blogs/page.tsx
@@ -1,24 +1,14 @@
+import React from 'react';
+import { BlogList } from '@/src/views/BlogList';
 import { getDictionary } from '@/src/utils/dictionaries';
 import type { Locale } from '@/src/utils/i18n';
-import { BlogList } from '@/src/views/BlogList';
 
 
-export async function generateMetadata({params}: { params: Promise<{ lang: string }>}) {
-    const { lang } = await  params;
 
-    return {
-        alternates: {
-            canonical: `https://itshiken.io.vn/${lang}/blogs`,
-            languages: {
-                vi: 'https://itshiken.io.vn/vi/blogs',
-                en: 'https://itshiken.io.vn/en/blogs',
-                ja: 'https://itshiken.io.vn/ja/blogs',
-            },
-        },
-    };
-}
-export default async function Page({params}:{params:Promise<{lang: string}>})  {
-    const {lang} = await  params;
+export default async function BlogPage({params}:{params:Promise<{lang: string}> }) {
+    const {lang} = await params;
     const t = await getDictionary(lang as Locale)
-  return <BlogList t={t} lang={lang} />;
+    return (
+        <BlogList t={t} lang={lang}/>
+    );
 }
